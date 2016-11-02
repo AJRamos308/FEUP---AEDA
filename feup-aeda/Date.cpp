ba801c2c0a2dfc26b490be2431ebb51873b98589
Miguel Fernandes <miguelmanofernandes@gmail.com>
2016-11-02 03:06:00 +0000

    Erro anterior fixed, basta usar unsigned long long. :+1:

--- feup-aeda/Date.cpp
+++ feup-aeda/Date.cpp
@@ -10,14 +10,12 @@ Date::Date(unsigned int hour, unsigned int minutes, unsigned int day, unsigned i
 	this->year = year;
 	compactDate = year * 10 ^ 8 + month * 10 ^ 6 + day * 10 ^ 4 + hour * 10 ^ 2 + minutes;
 }
-Date::Date(unsigned long int compactDate) {
+Date::Date(unsigned long long compactDate) {
 	this->compactDate = compactDate;
 	
 	string compactString = to_string(compactDate);
-	//201611012045
 	cout << compactString << endl;
 	year = stoi(compactString.substr(0, 4));
-	cout << year;
 
 }
 unsigned int Date::getHour() {
--- feup-aeda/Date.h
+++ feup-aeda/Date.h
@@ -6,11 +6,11 @@ using namespace std;
 class Date {
 private:
 	unsigned int hour, minutes, day, month, year;
-	unsigned long int compactDate;
+	unsigned long long compactDate;
 public:
 	Date();
 	Date(unsigned int hour, unsigned int minutes, unsigned int day, unsigned int month, unsigned int year);
-	Date(unsigned long int compactDate);
+	Date(unsigned long long compactDate);
 	unsigned int getHour();
 	unsigned int getMinutes();
 	unsigned int getDay();
